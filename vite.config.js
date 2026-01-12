6a924e15c1fc9837f8e0f8fd3ce26b344d60fc5d
Luis Virrueta <luis.virrueta.contacto@gmail.com>
2026-01-12 17:11:36 +0100

    Fix: cambiar minify de terser a esbuild para evitar errores de scope

--- vite.config.js
+++ vite.config.js
@@ -7,18 +7,7 @@ export default defineConfig({
   ],
   build: {
     target: 'es2015',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-        pure_funcs: ['console.log', 'console.info', 'console.debug'],
-        passes: 2
-      },
-      mangle: {
-        safari10: true
-      }
-    },
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
